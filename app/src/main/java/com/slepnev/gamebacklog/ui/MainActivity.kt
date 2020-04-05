90029059b0ca92372fb1b915507f53a765468f87
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-05 14:40:29 +0300

    Changed til background color to white

--- app/src/main/java/com/slepnev/gamebacklog/ui/MainActivity.kt
+++ app/src/main/java/com/slepnev/gamebacklog/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.slepnev.gamebacklog.ui
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -16,9 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
         }
     }
 
