708d5876d6f4682dfd6877d16080a2f0e6f208fa
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-05 14:22:13 +0300

    Finished the AddActivity layout with MaterialCardView

--- app/src/main/java/com/slepnev/gamebacklog/ui/MainActivity.kt
+++ app/src/main/java/com/slepnev/gamebacklog/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.slepnev.gamebacklog
+package com.slepnev.gamebacklog.ui
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.slepnev.gamebacklog.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
